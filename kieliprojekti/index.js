8d077437d51c0d8733602662b22fc1e8199f0d2d
Oskari Noppa <the.nobba@gmail.com>
2017-05-27 15:17:36 +0000

    Perusulkonäkö kasassa

--- kieliprojekti/index.js
+++ kieliprojekti/index.js
@@ -1,5 +1,15 @@
-/* global ace */
+/* global ace, Terminal */
 
 var editor = ace.edit("editori");
 editor.setTheme("ace/theme/monokai");
 editor.getSession().setMode("ace/mode/javascript");
+
+
+var komentorivi = $('#komentorivi').console({
+    promptLabel: '$ ',
+    commandHandle: function() {
+        return 'haa';
+    }
+});
+
+komentorivi.promptText('testi');
