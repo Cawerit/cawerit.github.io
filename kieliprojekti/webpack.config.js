f193e9c0e052d11da0e21da4b3a6d52fa7b5fd66
Oskari Noppa <the.nobba@gmail.com>
2017-05-29 05:35:24 +0300

    Optimoi JS julkaisua varten

--- kieliprojekti/webpack.config.js
+++ kieliprojekti/webpack.config.js
@@ -2,7 +2,21 @@ const
     path    = require('path'),
     webpack = require('webpack');
 
+const env = (process.env.NODE_ENV || 'dev').toLowerCase().trim();
+const isProd = env === 'prod' || env === 'production';
+if (isProd) {
+    console.log('Starting production build...');
+}
+
 const relative = (...p) => path.join(__dirname, ...p);
+
+const babelLoaderConfig = {
+    loader: 'babel-loader',
+    options: {
+        presets: ['env'],
+        plugins: []
+    }
+};
 
 module.exports = {
     entry: relative('index.js'),
@@ -15,7 +29,24 @@ module.exports = {
             relative('..', 'node_modules')
         ]
     },
+    module: {
+        rules: isProd ? [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: babelLoaderConfig
+            },
+            {
+                test: /node_modules(\/|\\)kieliprojekti(\/|\\).*\.js$/,
+                use: babelLoaderConfig
+            }
+        ] : [],
+    },
     plugins: [
         new webpack.IgnorePlugin(/^fs$/)
-    ]
+    ].concat(isProd ? [
+        // Production blugins:
+        new webpack.optimize.UglifyJsPlugin()
+
+    ] : [])
 };
