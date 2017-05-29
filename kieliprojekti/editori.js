7e41fe43e3942ac7c735b42172a1d20bac9183b4
Oskari Noppa <the.nobba@gmail.com>
2017-05-29 20:42:46 +0300

    Lisää syntax-hilighting

--- kieliprojekti/editori.js
+++ kieliprojekti/editori.js
@@ -1,20 +1,65 @@
 import { store, valitse } from './store.js';
-import { once } from 'lodash';
+import { once, debounce } from 'lodash';
+import CodeMirror from 'codemirror';
+import 'codemirror/addon/mode/simple';
+
 
 export default once(() => {
-    const editor = ace.edit('editori');
-    editor.setTheme('ace/theme/monokai');
-    editor.getSession().setMode('ace/mode/javascript');
-    // Disabloi deprecation-varoitus
-    editor.$blockScrolling = Infinity;
-    // Disabloi validointi
-    editor.getSession().setUseWorker(false);
+    const editor = CodeMirror(document.getElementById('editori'), {
+        mode: 'ö',
+        theme: 'monokai',
+        lineNumbers: true
+    });
+
+    function setSize() {
+        const h = $(window).height() - $('nav.navbar-default').height();
+        editor.setSize('100%', h);
+    }
+    $(window).on('resize', debounce(setSize, 300));
+    setSize();
 
     store
         .filter(valitse('VALITTU_ESIMERKKI'))
         .subscribe(({ val }) => {
             editor.setValue(val);
         });
+        
 
     return editor;
+});
+
+// Määritetään syntax-hilight ö-kielelle
+// docs: https://codemirror.net/demo/simplemode.html
+CodeMirror.defineSimpleMode('ö', {
+  // The start state contains the rules that are intially used
+  start: [
+    { regex: /--.*/, token: 'comment' },
+    { regex: /""/, token: 'string' },
+    { regex: /"[^"]/, token: 'string', next: 'string' },
+    { regex: /\b(.+)\s?(\(.*\))(\s?=)/, token: ['variable-3', 'variable-2', 'operator'] },
+    // Rules are matched in the order in which they appear, so there is
+    // no ambiguity between this one and the one above
+    { regex: /[-+\/*=]+|on:/, token: 'operator' },
+    { regex: /\b(kun|on|niin|tai|muutoin|infiksi)\b/, token: 'keyword' },
+    { regex: /\b(Tosi|Epätosi)\b/, token: 'atom' },
+    { regex: /eiMitään|[$@:]/, token: 'builtin' },
+    { regex: /(tämä\()(.*)(\))/, token: ['builtin', null, 'builtin'] },
+    { regex: /\s-?[0-9]+\b/i, token: 'number' }
+  ],
+
+  // Käsittely usean rivin teksteille
+  string: [
+      { regex: /\\/, token: 'string', next: 'string_escape' },
+      { regex: /[^"\\]+/, token: 'string' },
+      { regex: /"/, token: 'string', next: 'start' }
+  ],
+
+  string_escape: [
+    { regex: /./, token: 'string', next: 'string' }
+  ],
+  
+  meta: {
+    dontIndentStates: ['comment'],
+    lineComment: '--'
+  }
 });
