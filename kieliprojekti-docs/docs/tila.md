f3c87b507e80ddc156d67623392b7576c63316b6
Oskari Noppa <the.nobba@gmail.com>
2017-06-02 01:09:33 +0300

    Esimerkkidokumentaatiota

---
+++ kieliprojekti-docs/docs/tila.md
@@ -0,0 +1,3 @@
+# tila
+
+Blaa blaa
--- kieliprojekti-docs/index.js
+++ kieliprojekti-docs/index.js
@@ -1,9 +1,10 @@
 import allDocs from './all-docs.js';
+import debounce from 'lodash/debounce';
 
 const
     hashReg = /#\/*(.*)/,
-    docElements = parseDocElements();
-
+    docElements = parseDocElements(),
+    allKeys = Array.from(docElements.keys());
 
 function parseDocElements() {
     const r = new Map();
@@ -11,13 +12,13 @@ function parseDocElements() {
     allDocs.forEach(d => {
         const
             $d = $(`<div>${d}</div>`),
-            header = $d.find('h2').eq(0).text();
+            header = $d.find('h1').eq(0).text();
 
         if (!header) {
             throw new Error(`File "${$d.html()}" does not have a proper header.\nAdd "## Header" to the top of the file.`);
         }
                 
-        r.set(header, $d);
+        r.set(header.trim(), $d);
     });
 
     return r;
@@ -29,19 +30,68 @@ function setArticle(key) {
     $('#main').html(template);
 }
 
+function getLink(key) {
+    return '#' + encodeURIComponent(key);
+}
+
+function addLink(ul, key) {
+    return $('<li></li>')
+        .append(
+            $(`<a href="${getLink(key)}"></a>`)
+                .text(key)
+        )
+        .appendTo(ul);
+}
+
+function getDocKeyFromHash() {
+    const
+      hash = decodeURIComponent(location.hash || '').match(hashReg),
+      key = hash && hash[1].trim();
+
+    if (key && docElements.has(key)) {
+        return key;
+    } else {
+        const [[r]] = docElements;
+        return r;
+    }
+}
 
 $(function() {
 
-    const hash = (location.hash || '').match(hashReg);
-
-    let initialDoc;
-
-    if (hash && allDocs.has(hash)) {
-        initialDoc = hash;
-    } else {
-        [[initialDoc]] = docElements;
-    }
+    const initialDoc = getDocKeyFromHash()
 
     setArticle(initialDoc);
+
+    window.onhashchange = function() {
+        setArticle(getDocKeyFromHash());
+    };
+
+    const ul = $('#sidebar>ul');
+    for (let [key] of docElements) {
+        addLink(ul, key);
+    }
+
+    $('#search')
+        .on('input', debounce(function() {
+            const
+                $el = $(this),
+                val = $el.val();
+
+            let filtered;
+
+            if (val) {
+                const vals = val.split(' ');
+                
+                filtered =
+                    allKeys.filter(k =>
+                        vals.some(v => !!v && k.indexOf(v) !== -1)
+                    );
+            } else {
+                filtered = allKeys;
+            }
+
+            ul.html('');
+            filtered.forEach(k => addLink(ul, k));
+        }, 200));
 
 });
